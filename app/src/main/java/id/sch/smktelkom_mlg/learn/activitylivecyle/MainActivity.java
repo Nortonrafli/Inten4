00294aeff5f248f993c1eea489aadf65d2ba5a67
Norton <norton.nra27@gmail.com>
2016-11-03 22:01:43 +0700

    livecyle Music

--- app/src/main/java/id/sch/smktelkom_mlg/learn/activitylivecyle/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/activitylivecyle/MainActivity.java
@@ -1,13 +1,63 @@
 package id.sch.smktelkom_mlg.learn.activitylivecyle;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "LivecycleTag";
+    MediaPlayer mediaplayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mediaplayer = MediaPlayer.create(this, R.raw.song);
+        Log.d(TAG, "onCreate: ");
+
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart:");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mediaplayer.start();
+        Log.d(TAG, "onResume:");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mediaplayer.pause();
+        Log.d(TAG, "onPause: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop:");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart:");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mediaplayer != null)
+            mediaplayer.release();
+        Log.d(TAG, "onDestroy:");
+    }
+
 }
